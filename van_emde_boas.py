eb736daca0257f07558ccf52b7da3906603b8424
nallam <nr858090@dal.ca>
2025-02-03 22:24:16 -0400

    Add comments, remove redundancies

--- van_emde_boas.py
+++ van_emde_boas.py
@@ -1,45 +1,71 @@
 import math
 
 class Tree:
+    # create an empty van Emde Boas tree given the universe size
     def __init__(self, universe):
         self.u = universe
-        print("u = ", self.u)
+        
+        # the smallest cluster size is 2
+        # TODO: modify so that the size of each partition is dynamic
         if(self.u == 2):
             self.cluster = [0,0]
             self.summary = None
-            print("cluster = ", 0, 0)
+            
         else: 
+            # the size of the block = sqrt(u)
             block = int(math.sqrt(self.u)) 
             self.cluster = [None] * block
             
+            # recursively build sqrt(u) clusters each of size sqrt(u)
             for i in range(0, block):
                 self.cluster[i] = Tree(block)
-            print("summary: ")
-            self.summary = Tree(block)
             
+            # recursively build one summary for each sqrt(u) structures when u > 2
+            self.summary = Tree(block)
+    
+    # insert an element x into the tree
     def insert(self, x):
+        # handle erroneous input
+        if x >= self.u or x < 0: 
+            print("Element not in universe.")
+            return 
+        
+        # insert directly in base case
         if(self.u == 2):
-            print("inserting")
             self.cluster[x] = 1
+            
         else: 
             high = self.high(x)
             low = self.low(x)
-            print("high: ", high)
-            print("low: ", low)
+            
+            # recurse to insert
             self.cluster[high].insert(low)
+            
+            # recursively insert in summary, too
             if self.summary is not None: self.summary.insert(high)
     
+    # given an element x, check if it is the tree
     def membership(self, x):
+        # handle erroneous input
+        if x >= self.u or x < 0: 
+            print("Element not in universe.")
+            return 
+        
+        # check drectly in base case
         if self.u == 2: 
             return self.cluster[x] == 1
+        
         else: 
             high = self.high(x)
             low = self.low(x)
+            
+            # check if summary is 0 before bothering to check cluster
             if self.summary.membership(high) is False: 
                 return False
             else: 
                 return self.cluster[high].membership(low)
     
+    # TODO: under construction
     def successor(self, x):
         if self.u == 2: 
             return self.cluster[x] == 1
@@ -50,15 +76,19 @@ class Tree:
                 return False
             else: 
                 return self.cluster[high].membership(low)
-            
+    
+    # the index of the cluster in which x would be located
     def high(self, x):
         block = int(math.sqrt(self.u))
         return int(math.floor(x/block))
     
+    # the index of x's cluster (or x itself) within that cluster
     def low(self, x):
         block = int(math.sqrt(self.u))
         return (x % block)
     
+    # rudimentary display of tree
+    # TODO: make prettier
     def show(self):
         print("u = ", self.u)
         if(self.u == 2):
@@ -72,6 +102,7 @@ class Tree:
             print("summary: ")
             self.summary.show()
             
+# some test code       
 t = Tree(16)
 print("----")
 t.show()
